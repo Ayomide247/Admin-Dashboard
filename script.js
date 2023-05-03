10bc0881061b59ecd49302ae5eebd0e353e54578
Ayomide <bayomide247@gmail.com>
2023-05-03 06:01:02 +0100

    Added Dashboard and Chat page

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuBtn = document.getElementById('menu-btn')
+const sideBar = document.querySelector('.sidemenu')
+const sideBarOverlay = document.querySelector('.sidebar-overlay')
+
+
+const toggleSideNav = () => sideBar.classList.toggle('-translate-x-96')
+
+menuBtn.addEventListener('click', () => toggleSideNav())
+sideBarOverlay.addEventListener('click', () => toggleSideNav())
+
+
+// const profile = document.getElementById('profile')
+// const check = document.getElementById('check')
+
+// profile.addEventListener('mouseover', () => {
+//     // console.log(check)
+//     if (profile === 'mouseover') {
+        
+//         check.classList.add('hidden')
+//     }
+// })
